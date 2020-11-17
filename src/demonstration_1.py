e9919c109fc58ba860e4d629444d73807a6de422
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-11-17 10:06:36 -0700

    notes complete

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,17 +1,17 @@
 """
 Your task is create your own HashTable without using a built-in library.
-
+​
 Your HashTable needs to have the following functions:
-
+​
 - put(key, value) : Inserts a (key, value) pair into the HashTable. If the
 value already exists in the HashTable, update the value.
 - get(key): Returns the value to which the specified key is mapped, or -1 if
 this map contains no mapping for the key.
 - remove(key) : Remove the mapping for the value key if this map contains the
 mapping for the key.
-
+​
 Example:
-
+​
 ```plaintext
 hash_table = MyHashTable();
 hash_table.put("a", 1);
@@ -24,25 +24,111 @@ hash_table.remove("b");         // remove the mapping for 2
 hash_table.get("b");            // returns -1 (not found)
 ```
 """
+​
+# Used for when we implement linked list chaining to handle collisions 
 class ListNode:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
-
+​
 class MyHashTable:
-    def __init__(self):
+    def __init__(self, capacity=100):
         # Your code here
-
-
+        self.capacity = capacity
+        # initializing an array of None values that has length of `capacity` 
+        self.storage = [None for _ in range(self.capacity)]
+​
+    # returns an integer output that will be used to index into `self.storage` 
+    def _hash(self, input):
+        hash = id(input)
+        # we have to ensure that `hash` is within bounds of our storage 
+        return hash % self.capacity
+​
     def put(self, key, value):
         # Your code here
-
-
+        # 1. hash our key to figure out where in storage this key-value will go 
+        index = self._hash(key) 
+        # 2. once we know the index, check that that spot is available 
+        if self.storage[index] is None:
+        # 3. if it is, insert the key-value pair at that spot 
+            self.storage[index] = ListNode(key, value)
+        # otherwise, update the key-value pair at that spot 
+        else:
+            # there's already a linked list at this array slot 
+            # to add this key-value pair, we need to traverse the linked list 
+            # to get to the end, and then add the key-value pair to the tail 
+            # of our linked list 
+            current = self.storage[index]
+            # if we get to the end of the linked list, that means we never 
+            # saw a matching key in the linked list, so we'll go ahead and 
+            # add a new node to the end of this linked list 
+            while current is not None:
+                # while traversing our linked list, check each node's key 
+                # to see if it matches the input key 
+                # if it does, we need to overwrite that key's value instead 
+                # of inserting a new linked list node 
+                if current.key == key:
+                    current.value = value
+                    return 
+                # check if current.next is None
+                # if it is, add the new list node after current 
+                if current.next is None:
+                    current.next = ListNode(key, value)
+                    return 
+                current = current.next
+​
     def get(self, key):
         # Your code here
-
-
+        # 1. hash our key to figure out the spot where this key-value pair will
+        # be in our storage 
+        index = self._hash(key) 
+        # 2. check the index in self.storage to make sure that the key-value pair 
+        # actually exists at that spot 
+        if self.storage[index] is not None:
+            # traverse the linked list to try and find the key that matches the
+            # input key 
+            current = self.storage[index]
+            while current is not None:
+                if current.key == key:
+                    return current.value
+                current = current.next
+            # if we get to the end of the linked list, then no node's matched 
+            # the key we were looking for 
+        return -1 
+​
     def remove(self, key: int) -> None:
         # Your code here
-
+        # 1. hash our key to figure out the spot where this key-value pair will 
+        # be in our storage 
+        index = self._hash(key)
+        # 2. check the index in self.storage to make sure that the key-value pair 
+        # actually exists at that spot 
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            prev = None 
+            while current is not None:
+                if current.key == key:
+                    # check if prev is None 
+                    # if prev is None, that means that the first linked list node 
+                    # holding the key we're looking to remove 
+                    # set self.storage[index] = current.next 
+                    if prev is None:
+                        self.storage[index] = current.next 
+                        return 
+                    # set prev.next = current's next 
+                    prev.next = current.next 
+                    prev = current 
+                    current = current.next 
+            
+​
+hash_table = MyHashTable()
+hash_table.put("a", 1)
+hash_table.put("b", 2)
+print(hash_table.get("b"))
+print(hash_table.get("a"))         
+print(hash_table.get("c"))          
+hash_table.put("b", 1)         
+print(hash_table.get("b"))           
+hash_table.remove("b")        
+print(hash_table.get("b"))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -2,35 +2,35 @@
 You've uncovered a secret alien language. To your surpise, the language is made
 up of all English lowercase letters. However, the alphabet is possibly in a
 different order (but is some permutation of English lowercase letters).
-
+​
 You need to write a function that, given a sequence of words written in this
 secret language, and the order the alphabet, will determine if the given words
 are sorted "alphabetically" in this secret language.
-
+​
 The function will return a boolean value, true if the given words are sorted
 "alphabetically" (based on the supplied alphabet), and false if they are not
 sorted "alphabetically".
-
+​
 Example 1:
-
+​
 ```plaintext
 Input: words = ["lambda","school"], order = "hlabcdefgijkmnopqrstuvwxyz"
 Output: true
 Explanation: As 'l' comes before 's' in this language, then the sequence is
 sorted.
 ```
-
+​
 Example 2:
-
+​
 ```plaintext
 Input: words = ["were","where","yellow"], order = "habcdefgijklmnopqrstuvwxyz"
 Output: false
 Explanation: As 'e' comes after 'h' in this language, then words[0] > words[1],
 hence the sequence is unsorted.
 ```
-
+​
 Example 3:
-
+​
 ```plaintext
 Input: words = ["lambda","lamb"], order = "abcdefghijklmnopqrstuvwxyz"
 Output: false
@@ -39,9 +39,9 @@ shorter (in size.) According to lexicographical rules "lambda" > "lamb",
 because 'd' > '∅', where '∅' is defined as the blank character which is less
 than any other character (https://en.wikipedia.org/wiki/Lexicographic_order).
 ```
-
+​
 Notes:
-
+​
 - order.length == 26
 - All characters in words[i] and order are English lowercase letters.
 """
@@ -50,9 +50,49 @@ def are_words_sorted(words, alpha_order):
     Inputs:
     words: List[str]
     alpha_order: str
-
+​
     Output:
     bool
     """
     # Your code here
-
+    # if we didn't have to consider an alternate ordering, 
+    # we could just iterate through our words two-at-a-time and 
+    # check that every pair of words are in sorted order 
+    
+    # how do we change our logic to check for a different alphabetical ordering? 
+    # for each letter in the alphabet, we need to be able to know what comes after 
+    # this letter in the altered alphabetical ordering 
+    # one way we could do this is to store each letter with its index in the 
+    # altered ordering
+    # we'll init a dict to keep track of letters as keys and their altered indices 
+    # as values 
+    altered_order = {letter: index for index, letter in enumerate(alpha_order)} 
+    # that way, we can look up each letter in our dict to figure out its altered index
+    # once we have the altered indices we can check that they're in the right order 
+    
+    for i in range(1, len(words)):
+        word1 = words[i-1]
+        word2 = words[i]
+        # iterate through each of the characters of the two words 
+        # and check that the current letters adhere to the altered ordering 
+        for j in range(min(len(word1), len(word2))):
+            char1 = word1[j]
+            char2 = word2[j]
+            print(altered_order[char1], altered_order[char2])
+            # check each letter in both words
+            if char1 != char2:
+                if altered_order[char1] > altered_order[char2]:
+                    return False
+                else:
+                    break
+        
+        # if we get out of this loop, and len(word1) > len(word2)
+        # return False 
+        if len(word1) > len(word2):
+            return False
+​
+    return True 
+​
+​
+# print(are_words_sorted(["were","where","yellow"], "habcdefgijklmnopqrstuvwxyz"))
+print(are_words_sorted(["lambda","school"], "hlabcdefgijkmnopqrstuvwxyz"))
